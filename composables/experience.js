798a9d345a4308c6cd3f17c8abfa11c01f685600
tiagofort <tiagofortalezag@gmail.com>
2025-09-02 21:30:43 +0100

    experience section

--- composables/experience.js
+++ composables/experience.js
@@ -2,10 +2,10 @@ import { computed } from 'vue'
 
 export function useExperience (startYear = 2011) {
   const currentYear = new Date().getFullYear()
-  const totalYears = computed(() => currentYear - startYear + 1); // +1 pq conta o ano atual também
+  const totalYears = computed(() => currentYear - startYear)
 
   function getYearsOfExperience (fromYear) {
-    return Math.max(0, currentYear - fromYear + 1)
+    return Math.max(0, currentYear - fromYear)
   }
 
   return { totalYears, getYearsOfExperience}
