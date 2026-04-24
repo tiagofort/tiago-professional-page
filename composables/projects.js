8d25875ef48f08c1b630d74a83c5634a7834d3d3
tiagofort <tiagofortalezag@gmail.com>
2026-04-24 18:58:34 +0100

    Add AWS deployment workflow

--- composables/projects.js
+++ composables/projects.js
@@ -1,57 +1,58 @@
-import lunalogo from '../assets/images/luna-logo.webp';
-import tiagoprof from '../assets/images/tiago-profile.webp';
-import lunapp from '../assets/images/luna-app.webp';
-import ucdbapp from '../assets/images/ucdbapp.webp';
-import ucdb from '../assets/images/ucdb.webp';
+import lunalogo from "../assets/images/luna-logo.webp";
+import tiagoprof from "../assets/images/tiago-profile.webp";
+import lunapp from "../assets/images/luna-app.webp";
+import ucdbapp from "../assets/images/ucdbapp.webp";
+import ucdb from "../assets/images/ucdb.webp";
 
 export const projects = [
   {
-    title: 'Tiago Profile',
-    resume: 'My professional page developed using Vue3, Nuxt3 and Tailwind',
-    git: 'https://github.com/tiagofort/tiago-professional-page',
-    site: 'https://www.tiagofortgai.com',
+    title: "Tiago Profile",
+    resume: "My professional page developed using Vue3, Nuxt3 and Tailwind",
+    git: "https://github.com/tiagofort/tiago-professional-page",
+    site: "https://www.tiagofortgai.com",
     img: tiagoprof,
   },
   {
-    title: 'Luna Crystals API',
-    resume: 'Luna Crystals API developed in NodeJs and Express',
-    git: 'https://github.com/tiagofort/api-luna',
-    site: '',
+    title: "Luna Crystals API",
+    resume: "Luna Crystals API developed in NodeJs and Express",
+    git: "https://github.com/tiagofort/api-luna",
+    site: "",
     img: lunalogo,
   },
   {
-    title: 'Luna Crystals ADM APP',
-    resume: 'Luna Crystals Admin developed using Vue/Vuetify',
-    git: 'https://github.com/tiagofort/luna-adm-APP-exhibition',
-    site: 'https://luna-adm-app-exhibition-54f540dbf795.herokuapp.com/',
+    title: "Luna Crystals ADM APP",
+    resume: "Luna Crystals Admin developed using Vue/Vuetify",
+    git: "https://github.com/tiagofort/adm-APP-exhibition",
+    site: "https://luna-adm-app-exhibition-54f540dbf795.herokuapp.com/",
     img: lunalogo,
   },
   {
-    title: 'Luna Crystals APP',
-    resume: 'Luna Crystals APP developed using React, JavaScript and Taildwind',
-    git: 'https://github.com/tiagofort/luna-user-APP-exhibition',
-    site: 'https://www.luna-crystals.com',
+    title: "Luna Crystals APP",
+    resume: "Luna Crystals APP developed using React, JavaScript and Taildwind",
+    git: "https://github.com/tiagofort/luna-app",
+    site: "https://www.luna-crystals.com",
     img: lunapp,
   },
   {
-    title: 'Suicide intervention studies APP',
-    resume: 'This project is a simple, lightweight survey application built using React, vanilla CSS, and JavaScript',
-    git: 'https://github.com/tiagofort/ucdb',
-    site: 'https://ucdb.vercel.app/',
+    title: "Suicide intervention studies APP",
+    resume:
+      "This project is a simple, lightweight survey application built using React, vanilla CSS, and JavaScript",
+    git: "https://github.com/tiagofort/ucdb",
+    site: "https://ucdb.vercel.app/",
     img: ucdbapp,
   },
   {
-    title: 'Suicide intervention studies API',
-    resume: 'REST API built with Node.js and MongoDB',
-    git: 'https://github.com/tiagofort/ucbd_api',
-    site: '',
+    title: "Suicide intervention studies API",
+    resume: "REST API built with Node.js and MongoDB",
+    git: "https://github.com/tiagofort/ucbd_api",
+    site: "",
     img: ucdb,
   },
   {
-    title: 'Suicide intervention studies ADM APP',
-    resume: 'ADM page built using React, JavaScript and Tailwind',
-    git: 'https://github.com/tiagofort/ucbd_api',
-    site: '',
+    title: "Suicide intervention studies ADM APP",
+    resume: "ADM page built using React, JavaScript and Tailwind",
+    git: "https://github.com/tiagofort/ucbd_api",
+    site: "",
     img: ucdb,
   },
 ];
